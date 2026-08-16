ac08b7458d94e51e37680a5d6b438b6fe302397a
abhinandankumardube-byte <abhinandankumardube@gmail.com>
2026-08-16 09:16:02 +0530

    ✅ second-largest3735 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-16

---
+++ gfg/medium/second-largest3735/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+public int getSecondLargest(int[] arr) {
+    int n = arr.length;
+
+    int max = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int Smax = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > Smax && arr[i] != max) {
+            Smax = arr[i];
+        }
+    }
+
+    return Smax;
+}
+}

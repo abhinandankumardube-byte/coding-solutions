ebe01b9dd1a2d632d0ce22a1aa3d5584df22c5c6
abhinandankumardube-byte <abhinandankumardube@gmail.com>
2026-08-16 07:26:50 +0530

    ✅ Fruits Into Baskets II | Easy | Java | Runtime: 2 ms (81.94%) | Memory: 46.1 MB | 2026-08-16

---
+++ leetcode/easy/3477-fruits-into-baskets-ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int unplaced = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (fruits[i] <= baskets[j]) {
+                    baskets[j] = 0; 
+                    unplaced--;
+                    break;
+                }
+            }
+        }
+        return unplaced; 
+    }
+}

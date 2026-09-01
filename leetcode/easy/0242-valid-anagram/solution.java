3abf7680a62431049a72d0e6c4c268468e90ae35
abhinandankumardube-byte <abhinandankumardube@gmail.com>
2026-09-01 10:40:37 +0530

    ✅ Valid Anagram | Easy | Java | Runtime: 4 ms (83.52%) | Memory: 46.8 MB | 2026-09-01

--- leetcode/easy/0242-valid-anagram/solution.java
+++ leetcode/easy/0242-valid-anagram/solution.java
@@ -3,17 +3,8 @@ class Solution {
         if(s.length()!=t.length())return false;
         char a1[] = s.toCharArray();
         char a2[] = t.toCharArray();
-        for(int i=0;i<a1.length;i++){
-            boolean fl=false;
-            for(int j=0;j<a2.length;j++){
-                if(a1[i] == a2[j]){
-                    a2[j]=' ';
-                    fl=true;
-                    break;
-                }
-            }
-            if(fl==false)return false;
-        }
-        return true;
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1,a2);
     }
 }
